f6a2d380b41e2e43d5a4c80bc58a16b5cf00c301
David <yoxisem544@gmail.com>
2015-05-01 12:14:44 +0800

    Initial Commit

---
+++ ColorgySideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorgySideMenu
+//
+//  Created by David on 2015/5/1.
+//  Copyright (c) 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
